940247b0255bef667a0a8736a71a0ac40f48fafc
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2025-12-22 15:04:30 +0530

    [Aman] : Add AreaOfCircle calculation program

--- gcr-codebase/Programming Pre-requisites/AreaOfCircle.java
+++ gcr-codebase/Programming Pre-requisites/AreaOfCircle.java
@@ -1,8 +1,10 @@
+
 import java.util.Scanner;
 
 public class AreaOfCircle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("welcome to Java World");
 
         System.out.print("Enter the radius of the circle: ");
         double radius = sc.nextDouble();
