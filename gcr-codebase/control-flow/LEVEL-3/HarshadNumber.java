6947ef95aa202ed13faf909d7d3e73688bf99660
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2025-12-22 16:53:18 +0530

    [Aman] : Add HarshadNumber check program (Level 3)

--- gcr-codebase/control-flow/LEVEL-3/HarshadNumber.java
+++ gcr-codebase/control-flow/LEVEL-3/HarshadNumber.java
@@ -24,7 +24,8 @@ import java.util.*;
 	
 	public static void main(String args[])
 	{
-	   Scanner sc=new Scanner(System.in); 
+	   Scanner sc=new Scanner(System.in);
+		System.out.println("welcome to Java World");
 	  System.out.println(" Enter number ");
 
 	   int number=sc.nextInt();
