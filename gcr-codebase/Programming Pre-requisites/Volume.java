b5c09de5346318289e0e2b4a5566cc35629242c3
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2025-12-22 15:07:59 +0530

    [Aman] : Add Volume calculation program

--- gcr-codebase/Programming Pre-requisites/Volume.java
+++ gcr-codebase/Programming Pre-requisites/Volume.java
@@ -2,6 +2,7 @@ import java.util.*;
 class Volume{
 	public static void main(String args[]){
 		Scanner kb=new Scanner(System.in);
+		System.out.println("welcome to Java World");
 		double r=kb.nextDouble();
 		double h=kb.nextDouble();
 		System.out.println("Volume of cylinder " + Math.PI*(r*r)*h);
