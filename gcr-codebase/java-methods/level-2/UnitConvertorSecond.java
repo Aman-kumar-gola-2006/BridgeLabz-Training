6b733734f43303f3457db6131e169f5915c26726
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2025-12-22 15:42:41 +0530

    [Aman] : Add UnitConverter  2 program (Level 2)

--- gcr-codebase/java-methods/level-2/UnitConvertorSecond.java
+++ gcr-codebase/java-methods/level-2/UnitConvertorSecond.java
@@ -30,6 +30,7 @@ public class UnitConvertorSecond  {
 	
 	//taking scanner object
 		Scanner sc = new Scanner(System.in);
+		System.out.println("welcome to Java World");
 		//taking the user input for the km
 		System.out.println("Enter the yards");
 		int yards = sc.nextInt();
