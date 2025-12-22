a326e1f838cdbb425a8923583787b991a9f37d52
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2025-12-22 15:18:28 +0530

    [Aman] : Add CalculateDigitsFrequency program (Level 2)

--- gcr-codebase/arrays/LEVEL-2/CalculateDigitsFrequency.java
+++ gcr-codebase/arrays/LEVEL-2/CalculateDigitsFrequency.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class CalculateDigitsFrequency {
    public static void main(String[] args) {
+	   System.out.println("welcome to Java World");
 
       // Create a Scanner Object
       Scanner sc = new Scanner(System.in);
