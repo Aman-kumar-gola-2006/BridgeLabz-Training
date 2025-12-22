aad7842f86a67e86d2cc274aa3c5ba154acf6736
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2025-12-22 15:14:26 +0530

    [Aman] : Add MultiplicationTableRange program (Level 1)

--- gcr-codebase/arrays/LEVEL-1/MultiplicationTableRange.java
+++ gcr-codebase/arrays/LEVEL-1/MultiplicationTableRange.java
@@ -2,7 +2,7 @@ import java.util.Scanner;   // Import Scanner class for input
 
 public class MultiplicationTableRange {
     public static void main(String[] args) {
-
+        System.out.println("welcome to Java World");
         Scanner sc = new Scanner(System.in);  
         // Create Scanner object
 
