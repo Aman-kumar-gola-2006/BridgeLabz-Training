14360762f4238395a182debcc1d1c793bcfe42e1
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2025-12-22 14:38:39 +0530

    [Aman] : Add VolumeOfEarth calculation program (Level 1)

--- gcr-codebase/ programming-elements/LEVEL-1/volumeOfEarth.java
+++ gcr-codebase/ programming-elements/LEVEL-1/volumeOfEarth.java
@@ -2,7 +2,7 @@ public class volumeOfEarth{
 	public static void main(String [] args){
 		double radiusKm = 6378;
 		double pi = 3.14159265359;
-
+		System.out.println("welcome to Java World");
 		double volumeKm = (4/3) * pi * radiusKm * radiusKm * radiusKm;
 		double volumeMiles = volumeKm * Math.pow(0.621371, 3);
 
