d4ab626fdc19a6f26b09bccd5b783aabd2d12b89
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2025-12-22 15:06:23 +0530

    [Aman] : Add Distance calculation program

--- gcr-codebase/Programming Pre-requisites/Distance.java
+++ gcr-codebase/Programming Pre-requisites/Distance.java
@@ -2,7 +2,7 @@ import java.util.*;
 class ConversionOfDistance {
     public static void main(String[] args) {
         Scanner kb=new Scanner(System.in);
-        
+        System.out.println("welcome to Java World");
         double k = kb.nextDouble();
         double m = k*0.621371;
         System.out.println("Conversion of Distance from kilo meter to miles is : " + m);
