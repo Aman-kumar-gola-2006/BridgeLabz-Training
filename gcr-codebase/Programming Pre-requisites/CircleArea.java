7da402c06f9f0390d31ec1784f508dce88c99894
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2025-12-22 15:04:53 +0530

    [Aman] : Add CircleArea program

--- gcr-codebase/Programming Pre-requisites/CircleArea.java
+++ gcr-codebase/Programming Pre-requisites/CircleArea.java
@@ -2,6 +2,7 @@ import java.util.*;
 class CircleArea {
 	public static void main(String args[]){
 		Scanner kb=new Scanner(System.in);
+		System.out.println("welcome to Java World");
 		double r=kb.nextDouble();
 		System.out.println("Area of circle " + Math.PI*(r*r));
 	
