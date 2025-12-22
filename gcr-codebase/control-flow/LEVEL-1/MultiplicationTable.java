cf85e80743fb336c1f482fb2ca9bd62c2b523773
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2025-12-22 16:39:16 +0530

    [Aman] : Add MultiplicationTable program (Level 1)

--- gcr-codebase/control-flow/LEVEL-1/MultiplicationTable.java
+++ gcr-codebase/control-flow/LEVEL-1/MultiplicationTable.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class MultiplicationTable {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("welcome to Java World");
 
         System.out.println("Enter a number:");
         int number = sc.nextInt();
