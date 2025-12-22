e95e49cfb1dc7afde49b8ef8505332497cd2898e
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2025-12-22 15:07:25 +0530

    [Aman] : Add SimpleInterest calculation program

--- gcr-codebase/Programming Pre-requisites/SimpleInterest.java
+++ gcr-codebase/Programming Pre-requisites/SimpleInterest.java
@@ -2,6 +2,7 @@ import java.util.*;
 class SimpleInterest {
     public static void main(String[] args) {
         Scanner kb=new Scanner(System.in);
+        System.out.println("welcome to Java World");
         
         double p = kb.nextDouble();
         double r = kb.nextDouble();
