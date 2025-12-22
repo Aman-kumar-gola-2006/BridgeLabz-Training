aa9a4993ff618bb9be4fc6ced35ce306d09912d1
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2025-12-22 16:41:43 +0530

    [Aman] : Add SumNaturalNumbers program (Level 1)

--- gcr-codebase/control-flow/LEVEL-1/SumNaturalNumbers.java
+++ gcr-codebase/control-flow/LEVEL-1/SumNaturalNumbers.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class SumNaturalNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("welcome to Java World");
         System.out.println("Enter a number:");
         int n = sc.nextInt();
 
