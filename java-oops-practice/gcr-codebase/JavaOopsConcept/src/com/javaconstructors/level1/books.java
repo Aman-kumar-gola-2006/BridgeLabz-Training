1c1849db46b53ae2ca4e752680773f89f9b7e24b
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2025-12-28 22:20:07 +0530

    [Aman Kumar Gola] : added Books class for multiple book handling

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/javaconstructors/level1/books.java
@@ -0,0 +1,30 @@
+package com.javaconstructors.level1;
+
+public class books {
+
+	 // Public variable (accessible everywhere)
+    public String ISBN;
+
+    // Protected variable (accessible in subclass)
+    protected String title;
+
+    // Private variable (accessible only using methods)
+    private String author;
+
+    // Public constructor to initialize book details
+    public books(String ISBN, String title, String author) {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+    }
+
+    // Public method to set author name
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    // Public method to get author name
+    public String getAuthor() {
+        return author;
+    }
+}
