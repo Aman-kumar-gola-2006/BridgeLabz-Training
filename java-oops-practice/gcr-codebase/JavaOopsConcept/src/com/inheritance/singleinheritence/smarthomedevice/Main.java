50fa50830585d36e7174469eceb682eb3406673d
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2026-01-02 12:26:45 +0530

    [Aman kumar gola]: added Smart Home Devices Description: Create a hierarchy for a smart home system where Device is the superclass and Thermostat is a subclass.

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/inheritance/singleinheritence/smarthomedevice/Main.java
@@ -0,0 +1,12 @@
+package com.inheritance.singleinheritence.smarthomedevice;
+
+public class Main {
+	
+
+	    public static void main(String[] args) {
+
+	        Thermostat thermostat = new Thermostat("TH-101", true, 24);
+
+	        thermostat.displayStatus();
+	    }
+	}
