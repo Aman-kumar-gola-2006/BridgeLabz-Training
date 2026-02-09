2a265bcee50141210c2f18f7d843ceb970cfa99b
AMAN KUMAR GOLA <amanagola9841@gmail.com>
2026-02-09 12:48:45 +0530

    Delete gcr-codebase directory

--- gcr-codebase/Programming Pre-requisites/Add.java
+++
@@ -1,8 +0,0 @@
-class Add {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 12;
-        int sum = a + b;
-        System.out.println("sum is: " + sum);
-    }
-}
