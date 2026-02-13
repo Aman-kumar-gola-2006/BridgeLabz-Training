dbca23c28e66aa76c1c2e3b73239448aa82f0298
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2026-02-13 13:23:34 +0530

    [Aman Kumar Gola]: implemented data export feature with default methods in interfaces

---
+++ java8-programming-practice/gcr-Codebase/javaEightConcept/src/interfaces/defaultmethodsininterfaces/dataexportfeature/ReportExporter.java
@@ -0,0 +1,10 @@
+package interfaces.defaultmethodsininterfaces.dataexportfeature;
+
+public interface ReportExporter {
+
+    void export();
+
+    default void exportToJSON() {
+        System.out.println("Exporting report to JSON format");
+    }
+}
