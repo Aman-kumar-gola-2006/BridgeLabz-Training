c7db424f1a8bc4614399b3b08efa1b65bf4db9fd
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2026-02-16 14:48:20 +0530

    [Aman kumar gola] : added multi vehicle rental system (interface implementation)

---
+++ java8-programming-practice/gcr-Codebase/javaEightConcept/src/interfaces/implementinterfaces/multivehiclerental/Buses.java
@@ -0,0 +1,14 @@
+package interfaces.implementinterfaces.multivehiclerental;
+
+public class Buses implements Vehicle {
+
+	@Override
+	public void rent() {
+		System.out.println("Bus has been rented");
+	}
+
+	@Override
+	public void returnVehicle() {
+		System.out.println("Bus has been returned");
+	}
+}
---
+++ java8-programming-practice/gcr-Codebase/javaEightConcept/src/interfaces/implementinterfaces/multivehiclerental/Vehicle.java
@@ -0,0 +1,6 @@
+package interfaces.implementinterfaces.multivehiclerental;
+
+public interface Vehicle {
+	void rent();
+	void returnVehicle();
+}
