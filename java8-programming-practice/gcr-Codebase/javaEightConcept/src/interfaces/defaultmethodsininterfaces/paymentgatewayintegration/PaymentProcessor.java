d11b838cde872732061e5460575727d97e0b30a1
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2026-02-16 14:35:03 +0530

    [Aman kumar gola] : added payment gateway integration (default methods)

---
+++ java8-programming-practice/gcr-Codebase/javaEightConcept/src/interfaces/defaultmethodsininterfaces/paymentgatewayintegration/PaymentProcessor.java
@@ -0,0 +1,11 @@
+package interfaces.defaultmethodsininterfaces.paymentgatewayintegration;
+
+public interface PaymentProcessor {
+	
+	void pay(double amount);
+	
+	// New feature added later
+	default void refund(double amount) {
+		System.out.println("Refunding Amount : "+amount);
+	}
+}
