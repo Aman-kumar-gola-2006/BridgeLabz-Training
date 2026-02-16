a8b28781b26947e9e4c4b6eeb064ff8aeda3258e
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2026-02-16 14:39:20 +0530

    [Aman kumar gola] : added smart vehicle dashboard (default methods)

---
+++ java8-programming-practice/gcr-Codebase/javaEightConcept/src/interfaces/defaultmethodsininterfaces/smartvehicledashboard/Car.java
@@ -0,0 +1,9 @@
+package interfaces.defaultmethodsininterfaces.smartvehicledashboard;
+
+public class Car implements Vehicle {
+
+    @Override
+    public void displaySpeed() {
+        System.out.println("Car speed: 80 km/h");
+    }
+}
