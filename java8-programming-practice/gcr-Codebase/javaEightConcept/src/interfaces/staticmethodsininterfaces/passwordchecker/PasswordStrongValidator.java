43d154b931d0da0e2fc289ada3dbcd673691e4a8
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2026-02-16 14:49:04 +0530

    [Aman kumar gola] : added password checker (static interface methods)

---
+++ java8-programming-practice/gcr-Codebase/javaEightConcept/src/interfaces/staticmethodsininterfaces/passwordchecker/PasswordStrongValidator.java
@@ -0,0 +1,9 @@
+package interfaces.staticmethodsininterfaces.passwordchecker;
+
+public class PasswordStrongValidator {
+	public static void main(String[] args) {
+		String password = "236578DFGHJ";
+		
+		SecurityUtils.isPasswordStrong(password);
+	}
+}
